e807b5b76ee7e552954b0edb8e117ef8d3b33c92
BLKNeko <69521435+BLKNeko@users.noreply.github.com>
2021-08-24 20:15:14 -0300

    CORS test4

--- app.js
+++ app.js
@@ -15,21 +15,25 @@ app.use(express.urlencoded({ extended: false})) //APENAS DADOS SIMPLES
 
 //PERMIÇÃO DO CORS
 
+app.use(cors());
+
+/* 
 app.use((req, res, next) => {
     res.header('Acess-Control-Allow-Origin', '*');
     res.header('Acess-Control-Allow-Header', 'Content-Type, Origin, X-Requested-With, Authorization, Accept');
     res.header('Access-Control-Allow-Credentials',  true);
     res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 
-    /* if(req.method === 'OPTIONS'){
-        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    } */
+    // if(req.method === 'OPTIONS'){
+    //    res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+   //     return res.status(200).send({});
+   // } 
 
     app.use(cors());
     next();
 })
 
+ */
 
 app.use(morgan('dev'));
 
