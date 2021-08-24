d01c05e3ffb027a4f0c3be6a3d0524751e14d3e5
BLKNeko <69521435+BLKNeko@users.noreply.github.com>
2021-08-24 18:55:42 -0300

    CORS test2

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ app.use(express.urlencoded({ extended: false})) //APENAS DADOS SIMPLES
 app.use((req, res, next) => {
     res.header('Acess-Control-Allow-Origin', '*');
     res.header('Acess-Control-Allow-Header', 'Content-Type, Origin, X-Requested-With, Authorization, Accept');
-    res.header('Access-Control-Allow-Credentials',  'true');
+    res.header('Access-Control-Allow-Credentials',  true);
+    res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 
-    if(req.method === 'OPTIONS'){
+    /* if(req.method === 'OPTIONS'){
         res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).send({});
-    }
+    } */
 
     app.use(cors());
     next();
