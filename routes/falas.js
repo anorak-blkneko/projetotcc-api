777e37556c2d9b33b7f6ef5165ba87ffdd6a2b9f
BLKNeko <69521435+BLKNeko@users.noreply.github.com>
2021-08-24 17:52:40 -0300

    Update API Falas

--- routes/falas.js
+++ routes/falas.js
@@ -1,41 +1,82 @@
 const express = require('express');
 const router = express.Router();
+const client = require('../connection');
+
+client.connect();
 
 //RETORNA TODOS OS FALAS
 router.get('/', (req, res, next) =>{
-    res.status(200).send({
-        mensagem: "Usando GET para falas"
+
+    client.query(`Select * from falas`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
     });
+    client.end;
+
 });
 
 //RETORNA O FALAS NO ID
-router.get('/:id_usuario', (req, res, next) =>{
-    const id = req.param.id_usuario
-    res.status(200).send({
-        mensagem: "Usando GET para falas com ID",
-        ID: id
+router.get('/:id', (req, res, next) =>{
+
+    client.query(`Select * from falas where fala=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
     });
+    client.end;
+
 });
 
 //INSERI UM FALAS
 router.post('/', (req, res, next) =>{
-    res.status(201).send({
-        mensagem: "Usando POST para falas"
-    });
+    
+    const usuarios = req.body;
+    var textfala = req.body.text_fala;
+    let insertQuery = `insert into falas("text_fala") 
+                       values('${textfala}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message), console.log(textfala)}
+    })
+    client.end;
+
 });
 
 //ALTERA 
-router.patch('/', (req, res, next) =>{
-    res.status(201).send({
-        mensagem: "Usando PATCH para falas"
-    });
+router.patch('/:id', (req, res, next) =>{
+    
+    let user = req.body;
+    let updateQuery = `update falas
+                       text_fala = '${user.text_fala}'
+                       where id_fala = ${user.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
 });
 
 //DELETA
-router.delete('/', (req, res, next) =>{
-    res.status(201).send({
-        mensagem: "Usando DELETE para falas"
-    });
+router.delete('/:id', (req, res, next) =>{
+    
+    let insertQuery = `delete from falas where id_fala=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
 });
 
 
