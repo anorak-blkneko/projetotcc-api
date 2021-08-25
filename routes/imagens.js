4f634be75607f738f1ad3ad1dd8a6a699e029e11
Renan Barbosa <renan_barbosa91@hotmail.com>
2021-08-24 22:06:12 -0300

    Criado rotas de imagens e tutoriais

---
+++ routes/imagens.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../connection');
+
+client.connect();
+
+//GET DE TUDO
+router.get('/', (req, res, next) =>{
+
+    client.query(`Select * from imagens`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+
+});
+
+//GET ID ESPECIFICO
+router.get('/:id', (req, res, next) =>{
+
+    client.query(`Select * from imagens where id_img=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+
+});
+
+//INSERT
+router.post('/', (req, res, next) =>{
+    
+    const imagens = req.body;
+    let insertQuery = `insert into imagens("id_tutorial", "img_link") values(${imagens.id_tutorial}, '${imagens.img_link}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message), console.log(imagens.img_link)}
+    })
+    client.end;
+
+});
+
+//ALTER
+router.patch('/:id', (req, res, next) =>{
+    
+    let imagens = req.body;
+    let updateQuery = `update imagens
+                       id_tutorial = ${imagens.id_tutorial},
+                       img_link = '${imagens.img_link}'
+                       where id_img = ${imagens.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
+});
+
+//DELETE
+router.delete('/:id', (req, res, next) =>{
+    
+    let insertQuery = `delete from imagens where id_img=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
+});
+
+
+module.exports = router;
---
+++ routes/tutoriais.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../connection');
+
+client.connect();
+
+//GET DE TUDO
+router.get('/', (req, res, next) =>{
+
+    client.query(`Select * from tutoriais`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+
+});
+
+//GET ID ESPECIFICO
+router.get('/:id', (req, res, next) =>{
+
+    client.query(`Select * from tutoriais where id_tutorial=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+
+});
+
+//INSERT
+router.post('/', (req, res, next) =>{
+    
+    const tutoriais = req.body;
+    let insertQuery = `insert into tutoriais("nome_tutorial") values('${tutoriais.nome_tutorial}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message), console.log(tutoriais.nome_tutorial)}
+    })
+    client.end;
+
+});
+
+//ALTER
+router.patch('/:id', (req, res, next) =>{
+    
+    let tutoriais = req.body;
+    let updateQuery = `update tutoriais
+                       nome_tutorial = '${tutoriais.img_link}'
+                       where id_tutorial = ${tutoriais.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
+});
+
+//DELETE
+router.delete('/:id', (req, res, next) =>{
+    
+    let insertQuery = `delete from tutoriais where id_tutorial=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+
+});
+
+
+module.exports = router;
